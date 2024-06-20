479c9b4214454e08eaeff35399427bf759769a34
christian <christiandeveloper123@gmail.com>
2024-06-20 19:03:58 -0300

    portfolio

--- src/assets/ContactMe/CotactMe.jsx
+++ src/assets/ContactMe/CotactMe.jsx
@@ -4,13 +4,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
-
+import { useState } from 'react';
+import emaiJs from "@emailjs/browser"
 
 const ContactMe = () => {
   const { ref, inView } = useInView({ triggerOnce: true });
+  const [name, setName] = useState ("")
+  const [email, setEmail] = useState ("")
+  const [subject, setSubject] = useState ("")
+  const [message, setMessage ] = useState ('')
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+     if (name == "" || email == "" || subject == "" || message == ""){
+      alert ("Preencha todas informações!")
+      return
+     }
+     const templatParams = {
+      name: name,
+      message: message,
+      email: email
+     }
+     emaiJs.send("service_s6tpeob", "template_2m4qhbg", templatParams, "aGFyu7MBSgmGrQ4ee" )
+     .then((response)=> {
+      console.log ("email enviado", response.status, response.text)
+      setName ("")
+      setEmail("")
+      setMessage ("")
+      setSubject ("")
+     }, (err) => {
+      console.log ("erro", err)
+     })
+
+  }
 
 return (
-    <div ref={ref}>
+    <form onSubmit={sendEmail} ref={ref}>
       <div id='contact' className="contact">
         <motion.div 
           className="left"
@@ -33,14 +63,34 @@ return (
           animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 50 }} 
           transition={{ duration: 0.5, delay: 0.5 }} 
         >
-          <input placeholder='Enter Your Name' type="text" />
-          <input placeholder='Enter Your Email' type="email" />
-          <input placeholder='Enter Your Subject' type="subject" />
-          <textarea placeholder='Enter Your Message' name="" id=""></textarea>
-          <button className='submitButton' >Submit</button>
+          <input
+           placeholder='Enter Your Name' 
+           type="text" 
+           onChange={(e) => setName (e.target.value)}
+           value={name}
+          />
+          <input 
+          placeholder='Enter Your Email' 
+          type="email" 
+          onChange={(e) => setEmail (e.target.value)}
+          value={email}
+          />
+          <input 
+          placeholder='Enter Your Subject' 
+          type="subject"
+          onChange={(e) => setSubject (e.target.value)}
+          value={subject} 
+          />
+          <textarea 
+          placeholder='Enter Your Message'
+          onChange={(e) => setMessage (e.target.value)}
+          value={message} 
+          >
+          </textarea>
+          <button className='submitButton'>Submit</button>
         </motion.div>
       </div>
-    </div>
+    </form>
   )
 }
 
